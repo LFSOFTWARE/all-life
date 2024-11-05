61ff81e4b31caf20b80ba8abdccc568286622fb0
Luiz <luiz_Fernando-br2015@hotmail.com>
2024-11-04 23:00:18 -0300

    feat: adjust

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -15,28 +15,29 @@ export default function HomeContent() {
   const [age, setAge] = useState<string>("");
   const [colorMouseOver, setColorMouseOver] = useState<string>("");
   const [people, setPeople] = useState<Person[]>([
-  
+
   ]);
 
-function getRandomColor() {
-  const colors = [
-    '#5F9EA0', '#E9967A', '#008080', '#FFFFE0', '#FF4500', 
-    '#48D1CC', '#EEE8AA', '#D2691E', '#9370DB', '#FFEFD5', 
-    '#EE82EE', '#C0C0C0', '#3CB371', '#7CFC00', '#F08080', 
-    '#FFFFFF', '#FF69B4', '#C71585', '#8B4513', '#FFFF00', 
-    '#DAA520', '#FAFAD2', '#FAEBD7', '#FFD700', '#F0F8FF', 
-    '#778899', '#4169E1', '#556B2F', '#FFA500', '#66CDAA'
-  ];
-  
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
-}
+  function getRandomColor() {
+    const colors = [
+      '#5F9EA0', '#E9967A', '#008080', '#FFFFE0', '#FF4500',
+      '#48D1CC', '#EEE8AA', '#D2691E', '#9370DB', '#FFEFD5',
+      '#EE82EE', '#C0C0C0', '#3CB371', '#7CFC00', '#F08080',
+      '#FFFFFF', '#FF69B4', '#C71585', '#8B4513', '#FFFF00',
+      '#DAA520', '#FAFAD2', '#FAEBD7', '#FFD700', '#F0F8FF',
+      '#778899', '#4169E1', '#556B2F', '#FFA500', '#66CDAA'
+    ];
+
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
 
   const getColorByAge = (age: number): string | undefined => {
     const person = people.find((item: Person) => age <= item.age);
     return person?.color;
   };
   const handleAddPerson = () => {
+    if (!name || !age) return
     const newPerson: Person = { name, age: parseInt(age), color: getRandomColor() };
     const updatedPeople = [...people, newPerson].sort((a, b) => a.age - b.age);
     setPeople(updatedPeople);
@@ -46,7 +47,7 @@ function getRandomColor() {
 
   useEffect(() => {
     console.log(colorMouseOver);
-    
+
   }, [colorMouseOver]);
 
   return (
@@ -55,35 +56,52 @@ function getRandomColor() {
         <p style={{ marginBottom: "15px" }}>Week of Year ---{">"} </p>
         {matrix.map((row: any, rowIndex: number) => (
           <Line key={rowIndex}>
-            {(rowIndex % 5 === 0 || rowIndex === 0 || rowIndex === 89) ? <Year>{rowIndex}</Year> : <Year></Year>}
+            {(rowIndex % 5 === 0 || rowIndex === 0 || rowIndex === 89) ? <Year>{rowIndex }</Year> : <Year></Year>}
             {row.map((item: any, colIndex: number) => (
-              <Cell hig={getColorByAge(rowIndex) == colorMouseOver}  key={colIndex} color={getColorByAge(rowIndex)} />
+              <>
+                <Cell
+                  hig={getColorByAge(rowIndex) == colorMouseOver} key={colIndex} color={getColorByAge(rowIndex)}
+                  title={`Week: ${(rowIndex * matrix[0].length) + colIndex + 1}`}
+                  />
+
+              </>
             ))}
           </Line>
         ))}
       </ContainerTable>
       <Inputs>
-        {people.map((person: Person, index: number) => (
-          <ContainerNames key={index} onMouseOver={() => setColorMouseOver(person.color)} onMouseOut={() => setColorMouseOver("")}>
-            <Cell hig={person.color  == colorMouseOver} color={person.color} />
-            <p>{person.name}</p>
-            <p> {person.age}</p>
-          </ContainerNames>
-        ))}
+        <h2>Persons</h2>
 
-        <input
-          placeholder='Name'
-          value={name}
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <div>
-        <input
-          placeholder="Age"
-          type="text"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
+        <div className='list'>
+          {people.map((person: Person, index: number) => (
+            <ContainerNames key={index} onMouseOver={() => setColorMouseOver(person.color)} onMouseOut={() => setColorMouseOver("")}>
+              <div className='infos'>
+                <Cell hig={person.color == colorMouseOver} color={person.color} />
+                <p>{person.name}</p>
+                <p> {person.age}</p>
+              </div>
+              <div className='delete' onClick={() => setPeople(people.filter((_, i) => i !== index))}>
+                <p>&times;</p>
+              </div>
+            </ContainerNames>
+          ))}
+        </div>
+
+        <div className='inputs'>
+          <input
+            placeholder='Name'
+            value={name}
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <div>
+            <input
+              placeholder="Age"
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
         </div>
         <button type='button' onClick={handleAddPerson}>Add</button>
       </Inputs>
--- src/app/components/styled.ts
+++ src/app/components/styled.ts
@@ -14,9 +14,9 @@ justify-content: center;
 export const Cell = styled.div<PropsCell>`
   width: 20px;
   height: 20px;
-  border :${ props => props.hig ? '2px' : '1px' } solid black;
+  border :${props => props.hig ? '2px' : '1px'} solid black;
 
-  background-color:  ${ props => props.color ? props.color : 'white' };
+  background-color:  ${props => props.color ? props.color : 'white'};
   margin-right: 5px;
   margin-bottom: 5px;
 
@@ -42,11 +42,101 @@ export const Year = styled.p`
 `
 export const ContainerTable = styled.div``
 export const Inputs = styled.div`
+h2{
+  margin-bottom: 15px;
+}
 width: 10%;
 margin-top: 34px;
-margin-left: 2%;
+margin-left: 45px;
+display: flex;
+flex-direction: column;
+
+.list{
+  height: 200px;
+  max-height: 200px;
+  overflow-y: auto;
+}
+
+.list::-webkit-scrollbar {
+  width: 8px; /* Largura da barra de rolagem */
+}
+
+/* Estilizando a "track" da barra de rolagem */
+.list::-webkit-scrollbar-track {
+  background: #f1f1f1; /* Cor de fundo da track */
+  border-radius: 10px; /* Bordas arredondadas */
+}
+
+/* Estilizando o "thumb" da barra de rolagem */
+.list::-webkit-scrollbar-thumb {
+  background: #888; /* Cor do "thumb" */
+  border-radius: 10px; /* Bordas arredondadas */
+}
+
+/* Mudança de cor ao passar o mouse */
+.list::-webkit-scrollbar-thumb:hover {
+  background: #555; /* Cor do "thumb" ao passar o mouse */
+}
+.inputs{
+  margin-top: 25px;
+}
+input{
+  width: 100%;
+  height: 30px;
+  padding: 5px 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  outline: none;
+  margin-bottom: 8px;
+  transition: border-color 0.3s, box-shadow 0.3s;
+}
+input:focus {
+  border-color: #4a90e2;
+  box-shadow: 0 0 5px rgba(74, 144, 226, 0.5);
+}
+
+button {
+  background-color: #4a90e2;
+  color: white;
+  border: none;
+  margin-top: 10px;
+  border-radius: 5px;
+  padding: 8px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.2s;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+button:hover {
+  background-color: #357ab8;
+}
+
+button:active {
+  transform: scale(0.98);
+}
+
+button:focus {
+  outline: none;
+  box-shadow: 0 0 3px 2px rgba(74, 144, 226, 0.5);
+}
 `
 
 export const ContainerNames = styled.div`
 display: flex;
+justify-content: space-between;
+.infos{
+  display: flex;
+}
+
+.delete:hover{
+  cursor: pointer;
+  
+}
+
+.delete{
+  width: 15px;
+  height: 15px;
+}
 `
